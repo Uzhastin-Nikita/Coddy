b1944b9c9cc1d3b924696e1cd4ce6b3c41223724
Uzhastin-Nikita <fosfor20@mail.ru>
2022-01-21 21:13:59 +0300

    feat: addition js files

--- lectuer 7/script.js
+++ lectuer 7/script.js
@@ -1,1 +1,12 @@
-let a = 4;
+let n = parseInt(prompt("N"));
+
+if(n < 20){
+    alert(n * 0.2)
+    alert("Жкономии нет")
+}
+else if (n >= 20 && n < 400) {
+    let p = parseInt(n / 20);
+    let t = parseInt(n % 20);
+    alert(`Количество пачек; ${p}, ${t}`)
+    alert(`Выгода: ${n * 0.2 - (p*3.7 + t*0.2)}`);
+}
